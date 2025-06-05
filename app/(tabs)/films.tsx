72d3a77df7798f3255d536f9ad7521025e9c4f0a
Kint <kiloumanjaro@gmail.com>
2025-06-05 21:16:29 +0800

    films page changes

--- app/(tabs)/films.tsx
+++ app/(tabs)/films.tsx
@@ -1,10 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import UserReview from '~/components/ui/userreview';
+import { Text } from '~/components/ui/text';
+import { Button } from '~/components/ui/button';
+import { Plus } from 'lucide-react-native';
+import { useTheme } from '@react-navigation/native';
 import React from 'react'
 
+const review = {
+  title: 'Inception',
+  poster_path: '/poster.jpg',
+  review: 'Great movie!',
+  date: new Date(),
+  ratings: 8.4,
+};
+
 const films = () => {
+  const { colors } = useTheme();
+
   return (
-    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Text>films</Text>
+    <View className='flex-1 bg-secondary/30 gap-2 '>
+      <View className='px-4 flex-row mt-14 gap-2'>
+        <Button variant={"outline"} 
+          style={{
+            width: 40,       // or any size you prefer
+            height: 40,
+          }}
+        >
+          <Plus strokeWidth={1.5} size={20} color={colors.text}/>
+        </Button>
+
+        <Button variant={"outline"} 
+          style={{
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Reviews</Text>
+        </Button>
+
+        <Button variant={"outline"} 
+          style={{
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Movies</Text>
+        </Button>
+      </View>
+
+      <View className='p-2'>
+        <UserReview review={review} />
+      </View>
+    
     </View>
   )
 }
---
+++ components/ui/userreview.tsx
@@ -0,0 +1,89 @@
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '~/components/ui/accordion';
+import { Rating } from '@kolking/react-native-rating';
+import { useTheme } from '@react-navigation/native';
+import { Text } from '~/components/ui/text';
+import { FileText } from 'lucide-react-native';
+import { useRouter } from 'expo-router';
+
+interface UserReview {
+  title: string;
+  poster_path?: string;
+  review: string;
+  date: Date; 
+  ratings: number;
+}
+
+interface UserReviewProps {
+  review: UserReview;
+}
+
+const UserReview: React.FC<UserReviewProps> = ({ review }) => {
+  const [rating, setRating] = useState(0);
+  const { colors } = useTheme();
+  const isLongTitle = review.title.length > 27; // adjust threshold as needed
+  const handleChange = useCallback(
+    (value: number) => setRating(Math.round(value * 10) / 10),
+    []
+  );
+  
+  const router = useRouter();
+
+  return (
+    <Accordion
+      type="multiple"
+      defaultValue={['item-1']}
+      className="mt-1 ml-2 mr-2 mb-1"
+    >
+      <AccordionItem value="item-1" className="bg-card p-3 gap-3">
+        <AccordionTrigger className="pt-0 pb-0 pr-5 flex-row items-center">
+          <Image
+            source={{ uri: `https://image.tmdb.org/t/p/w500${review.poster_path}` }}
+            style={{ width: 63, height: 93, borderRadius: 3 }}
+            resizeMode="cover"
+          />
+          <View className={`ml-5 flex-1 ${isLongTitle ? 'h-[90%]' : 'h-3/4'} flex-col justify-start`}>
+            <View className="flex-grow">
+              <Text className='text-lg font-medium' style={{ lineHeight: 20 }}>{review.title}</Text>
+              <Text className='text-sm font-thin'>{(review.date).toLocaleDateString()}</Text>
+            </View>
+            <View className='flex-row gap-1'>
+              {review.ratings !== undefined && (
+                <Rating
+                  size={10}
+                  maxRating={5}
+                  rating={review.ratings / 2}
+                  onChange={handleChange}
+                  disabled={true}
+                  fillColor={colors.primary}
+                />
+              )}
+              <Text className='text-xs font-thin'>
+                ({review.ratings?.toFixed(1)})
+              </Text>
+            </View>
+          </View>
+        </AccordionTrigger>
+
+        <AccordionContent className="rounded-md p-4 bg-[#f8f8f8] dark:bg-[#222222] text-xl gap-3">
+          <View className='flex-row items-center gap-2'>
+            <FileText strokeWidth={1.5} size={18} color={colors.primary}/>
+            <Text className='text-base'>Review</Text>
+          </View>
+          <Text className='text-sm'>{review.review}</Text>
+        </AccordionContent>
+
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default UserReview;
