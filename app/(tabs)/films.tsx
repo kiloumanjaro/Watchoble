6d6daeb7d84417223e31700fbe7806efccee7121
Kint <kiloumanjaro@gmail.com>
2025-06-05 22:00:35 +0800

    Merge branch 'jr' of https://github.com/kiloumanjaro/Watchoble into ui-trial

--- app/(tabs)/films.tsx
+++ app/(tabs)/films.tsx
@@ -4,6 +4,7 @@ import { Text } from '~/components/ui/text';
 import { Button } from '~/components/ui/button';
 import { Plus } from 'lucide-react-native';
 import { useTheme } from '@react-navigation/native';
+import { Toggle, ToggleIcon } from '~/components/ui/toggle';
 import React from 'react'
 
 const review = {
@@ -14,47 +15,49 @@ const review = {
   ratings: 8.4,
 };
 
+
+
 const films = () => {
   const { colors } = useTheme();
+  const [pressedR, setPressedR] = React.useState(false);
+  const [pressedM, setPressedM] = React.useState(false);
+
 
   return (
     <View className='flex-1 bg-secondary/30 gap-2 '>
       <View className='px-4 flex-row mt-14 gap-2'>
         <Button variant={"outline"} 
           style={{
-            width: 40,       // or any size you prefer
-            height: 40,
+            width: 45,       // or any size you prefer
+            height: 45,
           }}
         >
           <Plus strokeWidth={1.5} size={20} color={colors.text}/>
         </Button>
 
-        <Button variant={"outline"} 
-          style={{
-            height: 40,
-
-
-          }}
+        <Toggle 
+          pressed={pressedR}
+          onPressedChange={setPressedR}
+          aria-label='Toggle bold'
+          variant='outline'
         >
           <Text>Reviews</Text>
-        </Button>
+        </Toggle>
 
-        <Button variant={"outline"} 
-          style={{
-            height: 40,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
+        <Toggle 
+          pressed={pressedM}
+          onPressedChange={setPressedM}
+          aria-label='Toggle bold'
+          variant='outline'
         >
           <Text>Movies</Text>
-        </Button>
+        </Toggle>
       </View>
-
-      <View className='p-2'>
-        <UserReview review={review} />
-      </View>
-    
+    <View className='p-2'>
+      <UserReview review={review} />
     </View>
+    
+  </View>
   )
 }
 
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -66,7 +66,7 @@ export default function Screen() {
               </AvatarFallback>
             </Avatar>
             <View className='p-3' />
-            <CardTitle className='pb-2 text-center'>Rick Sanchez</CardTitle>
+            <CardTitle className='pb-2 text-center'>{userdata?.username ?? 'user_101'}</CardTitle>
             <View className='flex-row'>
               <CardDescription className='text-base font-semibold'>Viewer</CardDescription>
               <Tooltip delayDuration={150}>
