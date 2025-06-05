864fd1eedf3f268683da76c6462a05d771ae9e09
Kint <kiloumanjaro@gmail.com>
2025-06-05 09:38:02 +0800

    added blur on spoilers feature

--- components/ui/reviewcard.tsx
+++ components/ui/reviewcard.tsx
@@ -1,9 +1,5 @@
 import { View } from 'react-native'
-import {
-    Card,
-    CardDescription,
-    CardFooter,
-} from '~/components/ui/card';
+import React, { useState } from 'react';
 import {
   Accordion,
   AccordionContent,
@@ -13,11 +9,13 @@ import {
 import { Text } from '~/components/ui/text';
 import { useTheme } from '@react-navigation/native';
 import { CircleUser } from 'lucide-react-native';
+import { BlurView } from 'expo-blur';
 
 type ReviewCardProps = {
   username: string;
   review: string;
   date: Date;
+  has_spoiler: boolean;
 }
 
 const getDaysAgo = (date: Date): string => {
@@ -29,31 +27,44 @@ const getDaysAgo = (date: Date): string => {
   return `${diffDays} days ago`;
 };
 
-
-const ReviewCard: React.FC<ReviewCardProps> = ({ username, review, date, }) => {
-    const { colors } = useTheme();
+const ReviewCard: React.FC<ReviewCardProps> = ({ username, review, date, has_spoiler}) => {
+  const { colors } = useTheme();
+  const [isOpen, setIsOpen] = useState<boolean>(!has_spoiler);
 
     return (
     <Accordion
       type="multiple"
-      className='px-5 pb-5'
-      defaultValue={['item-1']}
+      className="px-5 pb-5"
+      defaultValue={has_spoiler ? undefined : ['item-1']}
+      onValueChange={(values) => setIsOpen(values.includes('item-1'))}
     >
       <AccordionItem value='item-1'>
         <AccordionTrigger>
-          <View className='px-6 py-2 flex-row items-center gap-4'>
-          <View className='flex-col'>
-              <CircleUser strokeWidth={1.5} size={45} color={colors.text}/>
+          {has_spoiler && !isOpen && (
+          <View className="absolute top-0 left-0 right-0 bottom-0 z-10 overflow-hidden rounded-xl">
+            <BlurView
+              experimentalBlurMethod="dimezisBlurView"
+              intensity={50}
+              tint="systemChromeMaterialDark"
+              className="flex-1"
+            />
           </View>
-          <View className='flex-1 flex-col'>
-                  <Text className='text-xl font-bold'>{username}</Text>
-                  <Text className='text-xs font-light'>{getDaysAgo(date)}</Text>
-          </View>
+          )}
+          <View className="px-6 py-2 flex-row items-center gap-4 relative overflow-hidden">
+            <View className="flex-col z-20">
+              <CircleUser strokeWidth={1.5} size={45} color={colors.text} />
+            </View>
+            <View className="flex-1 flex-col z-20">
+              <Text className="text-xl font-bold">{username}</Text>
+              <Text className="text-xs font-light">{getDaysAgo(date)}</Text>
+            </View>
           </View>
         </AccordionTrigger>
+
         <AccordionContent className='pl-16 pr-10 pb-9 bg-red-60'>
           <Text>{review}</Text>
         </AccordionContent>
+
       </AccordionItem>
     </Accordion>
     );
